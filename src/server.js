b4c52aae5b75b142f99d50d0ea2659d2d413e82f
Azim Usmanov <Buchkirex@gmail.com>
2025-11-02 15:24:32 -0600

    Created and tested simple CRUD functions for db

--- src/server.js
+++ src/server.js
@@ -40,3 +40,45 @@ app.listen(PORT, () => {
 });
 
 module.exports = app;
+
+// Creating a new user
+// Post /users - add new user
+app.post("/users", async (req, res) => {
+    try {
+        const { user_name, last_name, email } = req.body
+        const result = await pool.query(
+            "INSERT INTO users (user_name, last_name, email) VALUES ($1, $2, $3) RETURNING *",
+            [user_name, last_name, email]
+        )
+        res.json(result.rows[0])
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+})
+
+// Updating a user
+// Put /users/:id
+app.put("/users/:id", async (req, res) => {
+    try{
+        const { id } = req.params
+        const { user_name, last_name, email } = req.body
+        const result = await pool.query(
+            "UPDATE users SET user_name = $1, last_name = $2, email = $3 WHERE id = $4 RETURNING *",
+            [user_name, last_name, email, id]
+        )
+        res.json(result.rows[0])
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+})
+
+ // DELETE /users/:id - delete user
+ app.delete("/users/:id", async (req, res) => {
+    try {
+        const { id } = req.params
+        await pool.query("DELETE FROM users where id = $1", [id])
+        res.json({ message: "User deleted successfully" })
+    }  catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+ })
